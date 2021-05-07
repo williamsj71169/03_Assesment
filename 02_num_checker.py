d052891e33de2f9cca7c3269a3878b26f044f84a
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-05-07 12:17:26 +1200

    end of 7.5.21

---
+++ 02_num_checker.py
@@ -0,0 +1,22 @@
+
+# checks that input is either a float or an integer that is more that zero.
+# takes in custom error messages.
+def num_check(question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+# main stuff
+get_int = num_check("How long is side 1?", "Please enter an number more than 0\n", float)
+
+get_cost = num_check("How long is side 2?", "Please enter an number more than 0\n", float)
